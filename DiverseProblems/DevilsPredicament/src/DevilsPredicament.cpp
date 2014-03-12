dfb43e755fd904dec2836f2fe8d938854c8728f7
claudiordgz <claudio.rdgz@gmail.com>
2014-03-12 06:41:38 -0600

    Food Model Created, Initial commit for the Homer Simpson problem

---
+++ DiverseProblems/DevilsPredicament/src/DevilsPredicament.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : DevilsPredicament.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdint.h>
+#include <string>
+using namespace std;
+
+int main() {
+	int i = 5;
+	auto e = i;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
